eb363c8ea6cb293deea8ddb8f0db5af854e7a8cb
faduzin <eric.fadul@gmail.com>
2023-11-18 19:19:38 -0300

    progresso

--- Lombinho.gd
+++ Lombinho.gd
@@ -1,58 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const AIR_FRICTION := 1
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction
+@export var SPEED : int = 150
+@export var JUMP_FORCE : int = 255
+@export var GRAVITY : int = 900
 
 func _physics_process(delta):
-	# Add the gravity.
-	velocity.x = 0
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("ui_left", "ui_right")
+	
+	var direction = Input.get_axis("Left", "Right")
+	
 	if direction:
-		velocity.x = lerp(velocity.x, direction * SPEED,AIR_FRICTION)
-		
+		velocity.x = direction * SPEED
+		if is_on_floor():
+			$AnimatedSprite2D.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if direction == -1:
-		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("Run")
+		velocity.x = 0
+		if is_on_floor():
+			$AnimatedSprite2D.play("default")
 	
+	#GRAVITY
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		
+	#DIRECTION
 	if direction == 1:
 		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("Run")
-	
-	if direction == 0:
-		$AnimatedSprite2D.play("default")
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h = true
 		
-	
+	#JUMP
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y -= JUMP_FORCE
+		$AnimatedSprite2D.play("Jump")
 	
 	move_and_slide()
-	
-	
-func _set_state():
-	var state = "defaut"
-		
-	if !is_on_floor():
-		state = "Jump"
-	elif  direction != 0:
-		state = "Run"
-		
-	if($AnimatedSprite2D.name != state):
-		$AnimatedSprite2D.play(state)
-		
-		
