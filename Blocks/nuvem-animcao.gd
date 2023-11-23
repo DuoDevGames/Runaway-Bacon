881c510094b4a8a0036d53a22179f3721b3cc85e
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2023-11-23 10:10:03 -0300

    feature: nuvens chovem ao serem tocadas

--- Blocks/nuvem-animcao.gd
+++ Blocks/nuvem-animcao.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$GPUParticles2D.emitting = false
 	pass # Replace with function body.
 
 
@@ -12,6 +13,10 @@ func _process(delta):
 
 func _on_body_entered(body):
 	$AnimatedSprite2D.play('angry')
+	$GPUParticles2D.emitting = true
 
 func _on_body_exited(body):
+	await get_tree().create_timer(5).timeout
+	$GPUParticles2D.emitting = false
 	$AnimatedSprite2D.play('happy')
+	
