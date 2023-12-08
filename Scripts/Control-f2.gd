17b631bca3f44b532275071ce848c3fd8426abcb
faduzin <eric.fadul@gmail.com>
2023-12-08 17:03:41 -0300

    feature: progress

--- Scripts/Control-f2.gd
+++ Scripts/Control-f2.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var num_coracao = $"MarginContainer/coracao-container/Label3" as Label
 @onready var tempo = $MarginContainer/timer/Label2 as Label
+@onready var player := $"../../Lombinho" as CharacterBody2D
 var alive := true
 var total_palhas = "teste " + str("%02d" % Global.palhas) + " / 10" 
 
@@ -14,3 +15,28 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	num_coracao.text = str("%02d" % Global.coracoes)
+
+var minutes = 0
+var seconds = 0
+var Dminutes = 1
+var Dseconds = 59
+
+func _on_timer_timeout():
+	if seconds == 0:
+		if minutes > 0:
+			minutes -= 1
+			seconds = 60
+	if seconds > 0:
+		seconds -= 1
+	
+	if minutes == 0:
+		if seconds == 0:
+			if alive:
+				alive = false
+				player.die()
+	
+	tempo.text = str(minutes) + ":" + str(seconds)
+
+func reset_timer():
+	seconds = Dseconds
+	minutes = Dminutes
