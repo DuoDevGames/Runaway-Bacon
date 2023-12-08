927eb35a0cdd028648c5e434d9a1d3b3de007476
faduzin <eric.fadul@gmail.com>
2023-12-08 16:35:22 -0300

    feature: progress

--- Scripts/Fase-02.gd
+++ Scripts/Fase-02.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var gos := $HUD/GameOverScreen
 @onready var player := $Lombinho as CharacterBody2D
 @onready var camera := $Camera2D as Camera2D
-@onready var teleporter_out = $"Teleporter-out"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +16,3 @@ func _process(_delta):
 		await get_tree().create_timer(1).timeout
 		gos.visible = true
 
-
-func _on_teleporter_body_entered(body):
-	body.position = teleporter_out.position
--- Scripts/hudmanager.gd
+++ Scripts/hudmanager.gd
@@ -1,20 +1,34 @@
 extends Control
 
-@onready var num_palha = $"MarginContainer/palha-container/Label3" as Label
 @onready var num_coracao = $"MarginContainer/coracao-container/Label3" as Label
+@onready var tempo = $MarginContainer/timer/Label2 as Label
 var player = null
 var total_palhas = "teste " + str("%02d" % Global.palhas) + " / 10" 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	num_palha.text = total_palhas
 	num_coracao.text = str("%02d" % Global.coracoes)
-	
+	reset_timer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	num_palha.text = str("%02d" % Global.palhas)
 	num_coracao.text = str("%02d" % Global.coracoes)
 
+var minutes = 0
+var seconds = 0
+var Dminutes = 1
+var Dseconds = 59
 
+func _on_timer_timeout():
+	if seconds == 0:
+		if minutes > 0:
+			minutes -= 1
+			seconds = 60
+	seconds -= 1
+	
+	tempo.text = str(minutes) + ":" + str(seconds)
+
+func reset_timer():
+	seconds = Dseconds
+	minutes = Dminutes
