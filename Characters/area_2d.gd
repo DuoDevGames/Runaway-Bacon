d7254c4666c4578cae25fa4d2e0b19d7526373ad
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-09-23 19:49:49 -0300

    Missão do Coitadinho

--- Characters/area_2d.gd
+++ Characters/area_2d.gd
@@ -12,5 +12,7 @@ func _on_body_entered(_body):
 			Dialogic.start("Coitadinho 1")
 			Global.primeiro_dialogo_coitadinho = 1
 	if Global.meia == 1:
-		Dialogic.start("Coitadinho 2")
-		Global.parede = false
+		if Global.primeiro_dialogo_coitadinho == 1:
+			Dialogic.start("Coitadinho 2")
+			Global.parede = false
+			Global.primeiro_dialogo_coitadinho = 2
