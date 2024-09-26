d0eeece7445e20ee8d7b15b2a60ed26b5f45cdb9
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-09-25 22:10:07 -0300

    hotfix: Plataforma movel

--- Scripts/nuvem_movel.gd
+++ Scripts/nuvem_movel.gd
@@ -16,17 +16,17 @@ func _ready() -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	#if(physics_proc == true):
 	plataform.position = plataform.position.lerp(follow, 0.5)
 	
 
 func move_platform():
-	var move_direction = Vector2.UP * distance
-	var duration = move_direction.length()/float(move_speed *plataform_center)
-	var platform_tween = create_tween().set_loops()
-	platform_tween.tween_property(self, 'follow', move_direction, duration).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)#.set_delay(wait_duration)
-	platform_tween.tween_property(self, 'follow', Vector2.ZERO, duration).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT).set_delay(wait_duration)
+	if(physics_proc == true):
+		var move_direction = Vector2.UP * distance
+		var duration = move_direction.length()/float(move_speed *plataform_center)
+		var platform_tween = create_tween().set_loops()
+		platform_tween.tween_property(self, 'follow', move_direction, duration).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)#.set_delay(wait_duration)
+		platform_tween.tween_property(self, 'follow', Vector2.ZERO, duration).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT).set_delay(wait_duration)
 
 
 #func _on_area_2d_body_entered(body: Node2D) -> void:
-#	physics_proc = true
+	
