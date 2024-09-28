684a9419b537eebed34caba7b5cc89a57c103811
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-09-28 19:39:46 -0300

    hotfix: Mostra o ícone de clique

--- Scripts/ninmu.gd
+++ Scripts/ninmu.gd
@@ -3,7 +3,7 @@ extends Area2D
 var player_is_on_area = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$"Balao Sprite".hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,8 +14,10 @@ func _process(delta: float) -> void:
 			player_is_on_area = false
 
 func _on_body_entered(body: Node2D) -> void:
+	$"Balao Sprite".show()
 	player_is_on_area = true
 
 
 func _on_body_exited(body: Node2D) -> void:
+	$"Balao Sprite".hide()
 	player_is_on_area = false
