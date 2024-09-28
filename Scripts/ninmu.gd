0aa94762bc86a8e09d3ab306159575a2d280e2d3
William Silva <121590646+williamoliveirasilva@users.noreply.github.com>
2024-09-28 15:02:39 -0300

    feature: Adição das mecânicas de Maritaca

---
+++ Scripts/ninmu.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var player_is_on_area = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if(player_is_on_area == true):
+		if(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+			Dialogic.start("Ninmu Infos")
+			player_is_on_area = false
+
+func _on_body_entered(body: Node2D) -> void:
+	player_is_on_area = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	player_is_on_area = false
